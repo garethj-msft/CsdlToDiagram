81f93660216915983331c07400b5c4f664b5a1e2
Gareth Jones <garethj@microsoft.com>
2020-10-25 14:40:18 -0700

    Remove dependency on CodeDom.

--- CsdlToPlant/CodeGeneratorBase.cs
+++ CsdlToPlant/CodeGeneratorBase.cs
@@ -3,7 +3,6 @@
     using System;
     using System.Collections.Generic;
     using System.Text;
-    using System.CodeDom.Compiler;
 
     /// <summary>
     /// Base class for generating well-formatted code.
@@ -21,7 +20,7 @@
         /// <summary>
         /// The error collection for the generation process.
         /// </summary>
-        public CompilerErrorCollection Errors { get; } = new CompilerErrorCollection();
+        public IList<GenerationError> Errors { get; } = new List<GenerationError>();
 
         /// <summary>
         /// A list of the lengths of each indent that were added with PushIndent.
@@ -45,8 +44,8 @@
 
             // If we're starting off, or if the previous text ended with a newline,
             // we have to append the current indent first.
-            if (((this.GenerationEnvironment.Length == 0)
-                 || this.endsWithNewline))
+            if (this.GenerationEnvironment.Length == 0
+                || this.endsWithNewline)
             {
                 this.GenerationEnvironment.Append(this.CurrentIndent);
                 this.endsWithNewline = false;
@@ -61,7 +60,7 @@
 
             // This is an optimization. If the current indent is "", then we don't have to do any
             // of the more complex stuff further down.
-            if ((this.CurrentIndent.Length == 0))
+            if (this.CurrentIndent.Length == 0)
             {
                 this.GenerationEnvironment.Append(textToAppend);
                 return;
@@ -69,12 +68,12 @@
 
             // Everywhere there is a newline in the text, add an indent after it
             textToAppend = textToAppend.Replace(global::System.Environment.NewLine,
-                (global::System.Environment.NewLine + this.CurrentIndent));
+                global::System.Environment.NewLine + this.CurrentIndent);
             // If the text ends with a newline, then we should strip off the indent added at the very end
             // because the appropriate indent will be added when the next time Write() is called
             if (this.endsWithNewline)
             {
-                this.GenerationEnvironment.Append(textToAppend, 0, (textToAppend.Length - this.CurrentIndent.Length));
+                this.GenerationEnvironment.Append(textToAppend, 0, textToAppend.Length - this.CurrentIndent.Length);
             }
             else
             {
@@ -113,8 +112,7 @@
         /// </summary>
         public void Error(string message)
         {
-            CompilerError error = new CompilerError();
-            error.ErrorText = message;
+            var error = new GenerationError {ErrorText = message};
             this.Errors.Add(error);
         }
 
@@ -123,7 +121,7 @@
         /// </summary>
         public void Warning(string message)
         {
-            CompilerError error = new CompilerError
+            var error = new GenerationError
             {
                 ErrorText = message,
                 IsWarning = true
@@ -136,12 +134,7 @@
         /// </summary>
         public void PushIndent(string indent)
         {
-            if ((indent == null))
-            {
-                throw new ArgumentNullException(nameof(indent));
-            }
-
-            this.CurrentIndent = (this.CurrentIndent + indent);
+            this.CurrentIndent += indent ?? throw new ArgumentNullException(nameof(indent));
             this.IndentLengths.Add(indent.Length);
         }
 
@@ -151,14 +144,14 @@
         public string PopIndent()
         {
             string returnValue = string.Empty;
-            if ((this.IndentLengths.Count > 0))
+            if (this.IndentLengths.Count > 0)
             {
-                int indentLength = this.IndentLengths[(this.IndentLengths.Count - 1)];
-                this.IndentLengths.RemoveAt((this.IndentLengths.Count - 1));
-                if ((indentLength > 0))
+                int indentLength = this.IndentLengths[this.IndentLengths.Count - 1];
+                this.IndentLengths.RemoveAt(this.IndentLengths.Count - 1);
+                if (indentLength > 0)
                 {
-                    returnValue = this.CurrentIndent.Substring((this.CurrentIndent.Length - indentLength));
-                    this.CurrentIndent = this.CurrentIndent.Remove((this.CurrentIndent.Length - indentLength));
+                    returnValue = this.CurrentIndent.Substring(this.CurrentIndent.Length - indentLength);
+                    this.CurrentIndent = this.CurrentIndent.Remove(this.CurrentIndent.Length - indentLength);
                 }
             }
 
---
+++ CsdlToPlant/GenerationError.cs
@@ -0,0 +1,72 @@
+﻿namespace CsdlToPlant
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    /// <summary>
+    /// POCO for errors during generation.
+    /// </summary>
+    public class GenerationError
+    {
+        private const string Warning = nameof(Warning);
+        private const string Error = nameof(Error);
+
+        /// <summary>Initializes a new instance of the <see cref="T:GenerationError" /> class.</summary>
+        public GenerationError()
+        {
+        }
+
+        /// <summary>Initializes a new instance of the <see cref="T:GenerationError" /> class using the specified file name, line, column, error number, and error text.</summary>
+        /// <param name="fileName">The file name of the file that the compiler was compiling when it encountered the error.</param>
+        /// <param name="line">The line of the source of the error.</param>
+        /// <param name="column">The column of the source of the error.</param>
+        /// <param name="errorNumber">The error number of the error.</param>
+        /// <param name="errorText">The error message text.</param>
+        public GenerationError(
+            string fileName,
+            int line,
+            int column,
+            string errorNumber,
+            string errorText)
+        {
+            this.FileName = fileName;
+            this.Line = line;
+            this.Column = column;
+            this.ErrorNumber = errorNumber;
+            this.ErrorText = errorText;
+        }
+
+        /// <summary>Gets or sets the column number where the source of the error occurs.</summary>
+        /// <returns>The column number of the source file where the compiler encountered the error.</returns>
+        public int Column { get; set; }
+
+        /// <summary>Gets or sets the error number.</summary>
+        /// <returns>The error number as a string.</returns>
+        public string ErrorNumber { get; set; }
+
+        /// <summary>Gets or sets the text of the error message.</summary>
+        /// <returns>The text of the error message.</returns>
+        public string ErrorText { get; set; }
+
+        /// <summary>Gets or sets the file name of the source file that contains the code which caused the error.</summary>
+        /// <returns>The file name of the source file that contains the code which caused the error.</returns>
+        public string FileName { get; set; }
+
+        /// <summary>Gets or sets a value that indicates whether the error is a warning.</summary>
+        /// <returns>
+        /// <see langword="true" /> if the error is a warning; otherwise, <see langword="false" />.</returns>
+        public bool IsWarning { get; set; }
+
+        /// <summary>Gets or sets the line number where the source of the error occurs.</summary>
+        /// <returns>The line number of the source file where the compiler encountered the error.</returns>
+        public int Line { get; set; }
+
+        /// <summary>Provides an implementation of Object's <see cref="M:System.Object.ToString" /> method.</summary>
+        /// <returns>A string representation of the compiler error.</returns>
+        public override string ToString()
+        {
+            return $"{this.FileName}:{this.Line},{this.Column} {(this.IsWarning ? Warning : Error)} {this.ErrorNumber} {this.ErrorText}";
+        }
+    }
+}
